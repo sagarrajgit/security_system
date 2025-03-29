c1f9a6a6f35623679ed3211cc495792cc79841c1
Sagar Raj <78961406+sagarrajgit@users.noreply.github.com>
2025-03-29 17:34:09 +0530

    Add files via upload
    uploaded initial implementation

---
+++ README.md
@@ -0,0 +1,22 @@
+# Security System
+
+## Overview
+This is a console-based security system implemented in C++ that provides basic user authentication features such as:
+
+- User Registration
+- User Login
+- Password Change
+- Program Exit
+
+User credentials (username and password) are stored in a file (file.txt), allowing persistence between program executions.
+
+## Usage <a name="usage"></a>
+1. **Compile the Code:**
+   ```bash
+   g++ -o security_system security_system.cpp
+   ```bash
+
+2. **Run the Program:**
+    ```bash
+    ./security_system
+    ```bash
---
+++ security_system.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    int a, i = 0;
+    string text, old, password1, password2, pass, name, password0, age, user, word, word1;
+    string creds[2], cp[2];
+    cout << "    Security System " << endl;
+
+    cout << ".____________________________." << endl;
+    cout << "|          1.Register        |" << endl;
+    cout << "|          2.login           |" << endl;
+    cout << "|          3.Change password |" << endl;
+    cout << "|          4.End Program     |" << endl;
+    cout << "|____________________________|" << endl;
+
+    do
+    {
+        cout << endl
+             << endl;
+        cout << "Enter your choice:- ";
+        cin >> a;
+        switch (a)
+        {
+        case 1:
+        {
+            cout << "|-----------Register-------------|" << endl;
+            cout << endl;
+            cout << "Please enter username:- ";
+            cin >> name;
+            cout << "Please enter the password:- " << "\n";
+            cin >> password0;
+            cout << "Please enter your age:- " << "\n";
+            cin >> age;
+
+            ofstream of1;
+            of1.open("file.txt");
+            if (of1.is_open())
+            {
+                of1 << name << "\n";
+                of1 << password0;
+                cout << "--------Registration successful--------" << endl;
+            }
+            break;
+        }
+        case 2:
+        {
+            i = 0;
+            cout << "|-----------Login-------------|" << endl;
+            cout << endl;
+            ifstream of2;
+            of2.open("file.txt");
+            cout << "Please enter the username:-  ";
+            cin >> user;
+            cout << "Please enter the password:- ";
+            cin >> pass;
+            if (of2.is_open())
+            {
+                while (!of2.eof())
+                {
+                    while (getline(of2, text))
+                    {
+                        istringstream iss(text);
+                        iss >> word;
+                        creds[i] = word;
+                        i++;
+                    }
+
+                    if (user == creds[0] && pass == creds[1])
+                    {
+                        cout << "--------log in successful--------";
+                        cout << endl
+                             << endl;
+
+                        cout << " Details: " << endl;
+                        cout << "Username: " + name << endl;
+                        cout << "password: " + pass << endl;
+                        cout << "Age: " + age << endl;
+                    }
+                    else
+                    {
+                        cout << endl
+                             << endl;
+                        cout << "|      Incorrect Credentials            |" << endl;
+                        cout << "|      1. Press 2 to login              |" << endl;
+                        cout << "|      2. Press 3 to Change password    |" << endl;
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+        case 3:
+        {
+            i = 0;
+            cout << "|-----------Change Password-----------|" << endl;
+            ifstream of0;
+            of0.open("file.txt");
+            cout << "enter the old password" << endl;
+            cin >> old;
+            if (of0.is_open())
+            {
+                while (of0.eof())
+                {
+                    while (getline(of0, text))
+                    {
+                        istringstream iss(text);
+                        iss >> word1;
+                        cp[i] = word1;
+                        i++;
+                    }
+                    if (old == cp[1])
+                    {
+                        of0.close();
+
+                        ofstream of1;
+                        if (of1.is_open())
+                        {
+                            cout << "enter your new password:- " << "\n";
+                            cin >> password1;
+                            cout << "Enter your password again:- " << "\n";
+                            cin >> password2;
+                            if (password1 == password2)
+                            {
+                                of1 << cp[0] << "\n";
+                                of1 << password1;
+                                cout << "Password change successfull" << "\n";
+                            }
+                            else
+                            {
+                                of1 << cp[0] << "\n";
+                                of1 << old;
+                                cout << "Password do not match" << "\n";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        cout << "Please enter the valid Password" << endl;
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+        case 4:
+        {
+            cout << "|-----------Thank You-----------|";
+            break;
+        }
+        default:
+            cout << "Enter a valid choice";
+        }
+    } while (a != 4);
+    return 0;
+}
